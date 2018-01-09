0054b29de2d6d958667ca1e6611d59a21a3cc58f
ac065455 <chen.anthony.1109@gmail.com>
2018-01-09 10:31:12 -0500

    first commit

---
+++ backend/my_app/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class Menu(models.Model):
+    menuid = models.PositiveIntegerField(max_length=11, null=False)
+    name = models.CharField(max_length=100, null=False)
+    subtext = models.CharField(max_length=100, default=None)
+    image = models.CharField(max_length=100, default=None)
+
+class Item(models.Model):
+    itemid = models.PositiveIntegerField(max_length=11, primary_key=True)
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, null=False)
+    description = models.CharField(max_length=200, default=None)
+    price1 = models.CharField(max_length=20, default=None)
+    price2 = models.CharField(max_length=20, default=None)
+    price3 = models.CharField(max_length=20, default=None)
+    sort = models.PositiveIntegerField(max_length=11, null=False, default=0)
