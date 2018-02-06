54323472e0def74ef1ac50cc7e3708b83fce1334
ac065455 <chen.anthony.1109@gmail.com>
2018-02-06 15:56:04 -0500

    added state to fetchdata

--- frontend/src/reducers/reducer_fetch.js
+++ frontend/src/reducers/reducer_fetch.js
@@ -1,10 +1,17 @@
 import { FETCH_MENU } from '../actions';
 
-export default function(state={}, action) {
+const initialState = {
+  menuItems: []
+}
+
+export default function(state=initialState, action) {
   switch(action.type) {
     case FETCH_MENU:
-      console.log(action.payload);
-      return {...state, menuItems: action.payload}
+    console.log(action.payload);
+      return {
+        ...state,
+        menuItems: action.payload
+      }
     default:
       return state;
   }
