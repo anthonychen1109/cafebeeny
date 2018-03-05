a44e3e6d1dff2bf04ed3ec0d65d5038ea9fcaedb
ac065455 <chen.anthony.1109@gmail.com>
2018-03-05 12:19:19 -0500

    added inputs for menu

--- frontend/src/containers/menu.js
+++ frontend/src/containers/menu.js
@@ -1,48 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-// Actions
-import { fetchMenu, fetchMenuId } from '../actions';
+import { fetchMenu, postMenuItem } from '../actions';
 
 class Menu extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      menuid: '',
-      newMenuId: '',
+      id: '',
       name: '',
       subtext: '',
-      images: ''
+      image: ''
     };
-    this.handleChange = this.handleChange.bind(this);
-    this.changeMenu = this.changeMenu.bind(this);
+    this.changeId = this.changeId.bind(this);
     this.changeName = this.changeName.bind(this);
     this.changeSubtext = this.changeSubtext.bind(this);
     this.changeImage = this.changeImage.bind(this);
-    this.setImage = this.setImage.bind(this);
+    this.save = this.save.bind(this);
   }
 
   componentDidMount() {
     this.props.fetchMenu();
   }
 
-  handleChange() {
-    let x = document.getElementById('idOfSelector');
-    let y = x[x.selectedIndex].value;
-    this.props.fetchMenuId(y);
-    this.setState({menuid: this.props.fetchmenuid.menuItems.menuid});
-    this.setState({name: this.props.fetchmenuid.menuItems.name});
-    this.setState({subtext: this.props.fetchmenuid.menuItems.subtext});
-    this.setState({images: this.props.fetchmenuid.menuItems.image});
-      this.setImage(this.props.fetchmenuid.menuItems.image);
-  }
-
-  setImage(x) {
-    this.setState({images: x})
-  }
-
-  changeMenu(e) {
-    this.setState({ menuid: e.target.value})
+  changeId(e) {
+    this.setState({ id: e.target.value})
   }
 
   changeName(e) {
@@ -54,46 +37,51 @@ class Menu extends Component {
   }
 
   changeImage(e) {
-    this.setState({ images: e.target.value})
+    this.setState({ image: e.target.value})
+  }
+
+  save() {
+    this.props.postMenuItem(this.state.id,this.state.name,this.state.subtext,this.state.image)
+    this.setState({ id: '', name: '', subtext: '', image: ''})
   }
 
   render() {
+    // console.log(this.props.getMenu);
     return (
-      <div className="container">
-        <div>
-          <select id="idOfSelector" onChange={this.handleChange}>
-            <option>Create an item:</option>
-            {this.props.fetchmenu.menuItems.map((item,index) => {
-              return (
-                <option id="optionValue" key={item+index} value={item.menuid}>
-                  {item.menuid}
-                </option>
-              )
-            })}
-          </select>
+      <div className="menu">
+        <h1>Menu</h1>
+        <div className="menu-container container">
+          <div className="menuInputs">
+            <span>Menu Id:</span>
+            <input onChange={this.changeId} value={this.state.id} />
+          </div>
+          <div className="menuInputs">
+            <span>Name:</span>
+            <input onChange={this.changeName} value={this.state.name} />
+          </div>
+          <div className="menuInputs">
+            <span>Subtext:</span>
+            <input onChange={this.changeSubtext} value={this.state.subtext} />
+          </div>
+          <div className="menuInputs">
+            <span>Image:</span>
+            <input onChange={this.changeImage} value={this.state.image} />
+          </div>
         </div>
-        <div className="input">
-          <p className="input-name">Menu id:</p> <input id="menuid" onChange={this.changeMenu} value={this.state.menuid} />
-        </div>
-        <div className="input">
-          <p className="input-name">Name:</p> <input onChange={this.changeName} value={this.state.name} />
-        </div>
-        <div className="input">
-          <p className="input-name">Subtext:</p> <input onChange={this.changesubtext} value={this.state.subtext} />
-        </div>
-        <div className="input">
-          <p className="input-name">Image:</p> <input onChange={this.changeImage} value={this.state.images}/>
-        </div>
+        <button
+          className="btn btn-secondary"
+          onClick={this.save}>
+          Save
+        </button>
       </div>
     )
   }
 }
 
-function mapStateToProps(state) {
+const mapStateToProps = (state) => {
   return {
-    fetchmenu: state.fetchmenu,
-    fetchmenuid: state.fetchMenuId
+    getMenu: state.getMenu
   }
 }
 
-export default connect(mapStateToProps, { fetchMenu, fetchMenuId })(Menu);
+export default connect(mapStateToProps, { fetchMenu, postMenuItem })(Menu);
