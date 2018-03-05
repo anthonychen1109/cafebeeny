8ec8f687f38003730d31cffece142633c939fbad
ac065455 <chen.anthony.1109@gmail.com>
2018-03-05 15:26:16 -0500

    no change

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -4,6 +4,7 @@ const BASE_URL = 'http://127.0.0.1:8000';
 
 export const FETCH_MENU = 'FETCH_MENU';
 export const POST_MENU_ITEM = 'POST_MENU_ITEM';
+export const FETCH_MENU_ITEM = 'FETCH_MENU_ITEM';
 
 export function fetchMenu() {
   const url = `${BASE_URL}/menu/`;
@@ -28,6 +29,17 @@ export function postMenuItem(id, name, subtext, image) {
 
   return {
     type: POST_MENU_ITEM,
+    payload: request
+  }
+}
+
+export function fetchMenuItem(id) {
+  const url = `${BASE_URL}/menu/${id}`;
+  const request = axios.get(url)
+    .then(response => response.data)
+    .catch(error => console.log(error));
+  return {
+    type: FETCH_MENU_ITEM,
     payload: request
   }
 }
